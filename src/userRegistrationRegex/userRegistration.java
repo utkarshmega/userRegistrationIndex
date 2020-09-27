9068e2bc90bfe24a1e57ebd5f9254400543f47b2
utkarshmega <utkarsheduc@gmail.com>
2020-09-27 14:14:40 +0530

    UC4 modified and completed

--- src/userRegistrationRegex/userRegistration.java
+++ src/userRegistrationRegex/userRegistration.java
@@ -9,35 +9,35 @@ public class userRegistration {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter first name");
-//		String fname = sc.next();
-//		
-//		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
-//		Matcher matcher = pattern.matcher(fname);
-//		if(matcher.matches())
-//			System.out.println("Valid First Name");
-//		else
-//			System.out.println("Invalid First Name");
-//		
-//		System.out.println("Enter Last name");
-//		String lname = sc.next();
-//		
-//		//Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{2,}");
-//		Matcher matcher1 = pattern.matcher(lname);
-//		if(matcher1.matches())
-//			System.out.println("Valid Last Name");
-//		else
-//			System.out.println("Invalid Last Name");
-//		
-//		System.out.println("Enter Email Address");
-//		String email = sc.next();
-//		String pattern_email = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-//		Pattern pattern2 = Pattern.compile(pattern_email);
-//		Matcher matches2 = pattern2.matcher(email);
-//		if(matches2.matches())
-//			System.out.println("Valid Email Address");
-//		else
-//			System.out.println("Invalid Email Address");
+		System.out.println("Enter first name");
+		String fname = sc.next();
+		
+		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		Matcher matcher = pattern.matcher(fname);
+		if(matcher.matches())
+			System.out.println("Valid First Name");
+		else
+			System.out.println("Invalid First Name");
+		
+		System.out.println("Enter Last name");
+		String lname = sc.next();
+		
+		//Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{2,}");
+		Matcher matcher1 = pattern.matcher(lname);
+		if(matcher1.matches())
+			System.out.println("Valid Last Name");
+		else
+			System.out.println("Invalid Last Name");
+		
+		System.out.println("Enter Email Address");
+		String email = sc.next();
+		String pattern_email = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+		Pattern pattern2 = Pattern.compile(pattern_email);
+		Matcher matches2 = pattern2.matcher(email);
+		if(matches2.matches())
+			System.out.println("Valid Email Address");
+		else
+			System.out.println("Invalid Email Address");
 		
 		System.out.println("Enter Phone number with country code");
 		String phn = sc.next();
