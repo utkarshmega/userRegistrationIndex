4276bebf77f1c512ed55849ee5bb3a2932d4e2c5
utkarshmega <utkarsheduc@gmail.com>
2020-09-27 11:58:29 +0530

    UC1 Completed

--- src/userRegistrationRegex/userRegistration.java
+++ src/userRegistrationRegex/userRegistration.java
@@ -1,10 +1,26 @@
 package userRegistrationRegex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class userRegistration {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to User Registration");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first name");
+		String fname = sc.next();
+		
+		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
+		Matcher matcher = pattern.matcher(fname);
+		if(matcher.matches())
+			System.out.println("Valid First Name");
+		else
+			System.out.println("Invalid First Name");
+		
+		
+		sc.close();
 
 	}
 
