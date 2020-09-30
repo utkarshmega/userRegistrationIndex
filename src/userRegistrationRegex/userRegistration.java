a1b639808f3246a56addc516f3660146a71a7acb
utkarshmega <utkarsheduc@gmail.com>
2020-09-30 23:57:06 +0530

    Exception handling done

--- src/userRegistrationRegex/userRegistration.java
+++ src/userRegistrationRegex/userRegistration.java
@@ -7,24 +7,24 @@ import java.util.regex.Pattern;
 public class userRegistration {
 	
 	
-	public String validateFname(String fname) {		
+	public String validateFname(String fname) throws userRegistrationException {		
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
 		Matcher matcher = pattern.matcher(fname);
 		if(matcher.matches())
 			return "Valid First Name";
 		else
-			return "Invalid First Name";
+			throw new userRegistrationException("Invalid fisrt name");
 	}
-	public String validateLname(String lname) {
+	public String validateLname(String lname) throws userRegistrationException {
 		
 		Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}");
 		Matcher matcher1 = pattern.matcher(lname);
 		if(matcher1.matches())
 			return "Valid Last Name";
 		else
-			return "Invalid Last Name";
+			throw new userRegistrationException("Invalid Last Name");
 	}
-	public String validateEmail(String email) {
+	public String validateEmail(String email) throws userRegistrationException {
 
 		String pattern_email = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
 		Pattern pattern2 = Pattern.compile(pattern_email);
@@ -32,9 +32,9 @@ public class userRegistration {
 		if(matches2.matches())
 			return "Valid Email Address";
 		else
-			return "Invalid Email Address";
+			throw new userRegistrationException("Invalid Email Address");
 	}
-	public String validatePhno(String phn) {
+	public String validatePhno(String phn) throws userRegistrationException {
 	
 		String pattern_phn = "[0-9]{2}\\s[1-9]{1}[0-9]{9}$";
 		Pattern pattern3 = Pattern.compile(pattern_phn);
@@ -42,9 +42,9 @@ public class userRegistration {
 		if(matcher3.matches())
 			return "Valid Phone Number";
 		else
-			return "Invalid Phone Number";
+			throw new userRegistrationException("Invalid Phone Number");
 	}
-	public String validatePassword(String password) {
+	public String validatePassword(String password) throws userRegistrationException {
 		
 		String pass_pattern = "(?=.*[0-9])(?=.*[!@#$%^&*|'<>.-^*()%!])(?=.*[A-Z])[^\\s]{8,}$";
 		Pattern pattern4 = Pattern.compile(pass_pattern);
@@ -52,7 +52,7 @@ public class userRegistration {
 		if(matcher4.matches())
 			return "Valid Password";
 		else
-			return "Invalid Password";
+			throw new userRegistrationException("Invalid Password");
 
 	}
 
---
+++ src/userRegistrationRegex/userRegistrationException.java
@@ -0,0 +1,10 @@
+package userRegistrationRegex;
+
+public class userRegistrationException extends Exception{
+	
+	public userRegistrationException(String msg)
+	{
+		super(msg);
+	}
+
+}
